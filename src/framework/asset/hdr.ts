6de05a6862a00d3406957a18bfce8ed6f7827fc5
MetaYoyo <olymyoyo@gmail.com>
2022-06-30 13:31:24 +0800

    update license

--- src/framework/asset/hdr.ts
+++ src/framework/asset/hdr.ts
@@ -1,6 +1,6 @@
 /**
 @license
-Copyright 2022 meta4d.me Authors
+Copyright (c) 2022 meta4d.me Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
